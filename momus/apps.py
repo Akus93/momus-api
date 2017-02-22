a2aaf22c3e22d723b4c0c7877bf9f72dbca0b90c
Akus93 <dawid.rdzanek@protonmail.com>
2017-02-22 17:16:27 +0100

    Init commit

---
+++ momus/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MomusConfig(AppConfig):
+    name = 'momus'
---
+++ momus_api/urls.py
@@ -0,0 +1,16 @@
+from django.conf.urls import url, include
+from django.contrib import admin
+from django.conf.urls.static import static
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
+from momus_api import settings
+
+urlpatterns = [
+    url(r'api/', include('momus.urls')),
+    url(r'^auth/', include('rest_auth.urls')),
+    url(r'^auth/registration/', include('rest_auth.registration.urls')),
+    url(r'^admin/', admin.site.urls),
+]
+
+urlpatterns += staticfiles_urlpatterns()
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
